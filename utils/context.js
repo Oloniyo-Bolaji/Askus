bafab83a8f29e6a8b922a04272f0863c71ea986e
Oloniyo-Bolaji <ayoolabolaji12@yahoo.com>
2025-05-01 15:16:18 +0100

    signup

--- utils/context.js
+++ utils/context.js
@@ -1,12 +1,27 @@
 'use client'
 
 import { createContext, useState, useEffect } from 'react';
-import { signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth'
-import { auth, googleProvider } from './firebase.js'
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth'
 import {useRouter} from 'next/navigation';
 
-const NextContext = createContext();
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: "G-NZC3XXWVR7"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
+
+
+const NextContext = createContext();
 
 const NextProvider = ({ children }) => {
   const router= useRouter()
