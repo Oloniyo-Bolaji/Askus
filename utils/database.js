df6b49314efe50c8b669df393de56d4405370451
Oloniyo-Bolaji <ayoolabolaji12@yahoo.com>
2025-05-01 14:30:15 +0100

    database resolved

--- utils/database.js
+++ utils/database.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 export const connecttoDb = async () => {
   mongoose.set('strictQuery', true)
   try{
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_CONNECT_STRING)
-    
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('Connected to Database')
   }catch(error){
     console.log(error)
   }
